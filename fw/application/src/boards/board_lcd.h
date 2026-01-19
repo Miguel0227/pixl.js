986b3b57a59c1bbf6cb9a283df75e9945f317a3e
Miguel0227 <duarteaguilarmiguelangel@gmail.com>
2026-01-18 18:56:20 -0600

    Update board_lcd.h
    Support to 4 buttons on Joysfusion amiibo

--- fw/application/src/boards/board_lcd.h
+++ fw/application/src/boards/board_lcd.h
@@ -1,46 +1,58 @@
 #ifndef BOARD_LCD_H
 #define BOARD_LCD_H
 
-#define LEDS_NUMBER    1
+#define LEDS_NUMBER 1
 
-#define LED_START      31
-#define LED_1          31
-#define LED_STOP       31
+#define LED_START 31
+#define LED_1 31
+#define LED_STOP 31
 
 #define LEDS_ACTIVE_STATE 0
 
-#define LEDS_INV_MASK  LEDS_MASK
+#define LEDS_INV_MASK LEDS_MASK
 
-#define LEDS_LIST { LED_1 }
+#define LEDS_LIST {LED_1}
 
-#define BSP_LED_0      LED_1
+#define BSP_LED_0 LED_1
 
+// ==========================================
+// CORRECCIÓN DE BOTONES PARA JOYSFUSION (4 TECLAS)
+// ==========================================
 
-#define BUTTONS_NUMBER 3
+// 1. Cambiamos la cantidad de botones a 4
+#define BUTTONS_NUMBER 4
 
-#define BUTTON_START   5
-#define BUTTON_1       5
-#define BUTTON_2       6
-#define BUTTON_3       7
-#define BUTTON_STOP    7
-#define BUTTON_PULL    NRF_GPIO_PIN_PULLUP
+#define BUTTON_START 5
+
+// 2. Aquí asignamos los PINES físicos.
+// He reordenado los números basándome en tu problema para intentar corregir el orden.
+// Si no quedan perfectos a la primera, solo tienes que cambiar estos números (5, 6, 7, 8) de lugar.
+
+#define BUTTON_1 5 // Tu Tecla 1 (Antes iba arriba, intentaremos que sea Atrás)
+#define BUTTON_2 6 // Tu Tecla 2 (OK - Esta estaba bien)
+#define BUTTON_3 8 // Tu Tecla 3 (Antes iba abajo. Probamos el PIN 8 aquí)
+#define BUTTON_4 7 // Tu Tecla 4 (La que estaba muerta. Probamos el PIN 7 aquí)
+
+#define BUTTON_STOP 8
+#define BUTTON_PULL NRF_GPIO_PIN_PULLUP
 
 #define BUTTONS_ACTIVE_STATE 0
 
-#define BUTTONS_LIST { BUTTON_1, BUTTON_2, BUTTON_3}
+// 3. Agregamos el BUTTON_4 a la lista
+#define BUTTONS_LIST {BUTTON_1, BUTTON_2, BUTTON_3, BUTTON_4}
 
-#define BSP_BUTTON_0   BUTTON_1
-#define BSP_BUTTON_1   BUTTON_2
-#define BSP_BUTTON_2   BUTTON_3
-
+// 4. Asignamos los botones lógicos
+#define BSP_BUTTON_0 BUTTON_1
+#define BSP_BUTTON_1 BUTTON_2
+#define BSP_BUTTON_2 BUTTON_3
+#define BSP_BUTTON_3 BUTTON_4
 
 // display config
-//#define OLED_SCREEN
-//#define OLED_SH1106
+// #define OLED_SCREEN
+// #define OLED_SH1106
 
 #define LCD_SCREEN
 #define LCD_ST7567
-
 
 // screen drivers
 #define LCD_CS_PIN 27
@@ -49,11 +61,9 @@
 #define LCD_DC_PIN 28 // green wire (DC)
 #define LCD_DEFAULT_CONTRAST 128
 
-
 // VFS driver
-//#define VFS_LFS_ENABLE
+// #define VFS_LFS_ENABLE
 #define VFS_SPIFFS_ENABLE
-
 
 #define NRFX_SPIM_SCK_PIN 26  // yellow wire (CLK)
 #define NRFX_SPIM_MOSI_PIN 25 // blue wire (DIN)
